a4bd37e7a732ab948e2de4a2d023d42c756f2d57
Victor Marroquin <victormarroquin469@gmail.com>
2024-10-09 15:33:15 -0600

    Bancalux-main directory delete

--- Bancalux-main/Controlador/cerrarCaj.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    session_start();
-    session_destroy();
-
-    header("location: ../Vista/logCajero.php");
-    exit();
-    
-?>
--- Bancalux-main/Vista/js/menuDesple.js
+++
@@ -1,4 +0,0 @@
-let mostrar = document.getElementById('mostrar');
-let menu = document.getElementById('menu');
-
-mostrar.addEventListener('click', function(){ menu.classList.toggle('mostrar'); mostrar.classList.toggle('move') })
