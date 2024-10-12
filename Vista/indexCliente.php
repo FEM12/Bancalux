adc752ff2ae6ba65acc6d20485eda3fe939fec4e
FloppaCoder <marcelo.menjivar2006@gmail.com>
2024-10-12 17:11:24 -0600

    Index Cliente Responsive Fix

--- Vista/indexCliente.php
+++ Vista/indexCliente.php
@@ -25,59 +25,65 @@ $resultados2 = $stmt2 -> fetchAll(PDO::FETCH_ASSOC);
 <html lang="es">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- Importar Tailwind CSS desde CDN -->
-  <script src="https://cdn.tailwindcss.com"></script>
-  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <!-- Importar Tailwind CSS desde CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
-  <title>Dashboard - Cliente</title>
+    <title>Dashboard - Cliente</title>
 </head>
 
 <style>
-  ::-webkit-scrollbar{width: 0;}
+::-webkit-scrollbar {
+    width: 0;
+}
 
-  body {
+body {
     background-image: radial-gradient(circle at 85% 32%, rgba(22, 78, 99, 0.2) 15%, transparent 35%),
-      radial-gradient(circle at 27% 51%, rgba(22, 78, 99, 0.5) 1%, transparent 50%),
-      radial-gradient(circle at 50% 50%, rgba(22, 78, 99, 0.1) 10%, transparent 50%);
-  }
+        radial-gradient(circle at 27% 51%, rgba(22, 78, 99, 0.5) 1%, transparent 50%),
+        radial-gradient(circle at 50% 50%, rgba(22, 78, 99, 0.1) 10%, transparent 50%);
+}
 </style>
 
 <body class="bg-slate-900 bg-fixed bg-cover min-h-screen flex flex-col items-center">
 
-  <nav class="bg-slate-950 bg-opacity-0 w-full p-4">
-    <ul class="flex justify-between items-center text-zinc-50 text-lg">
-      <li class="flex items-center font-bold text-2xl">
-        <div class="relative inline-block w-[2.5rem] h-[2.5rem] mr-2">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="absolute inset-0 text-zinc-200 opacity-40 w-full h-full" viewBox="0 0 16 16">
-            <path d="m8 0 6.61 3h.89a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v7a.5.5 0 0 1 .485.38l.5 2a.498.498 0 0 1-.485.62H.5a.498.498 0 0 1-.485-.62l.5-2A.5.5 0 0 1 1 13V6H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 3h.89zM3.777 3h8.447L8 1zM2 6v7h1V6zm2 0v7h2.5V6zm3.5 0v7h1V6zm2 0v7H12V6zM13 6v7h1V6zm2-1V4H1v1zm-.39 9H1.39l-.25 1h13.72z" />
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="absolute inset-0 bottom-4 m-auto w-[1.2rem] h-[1.2rem] text-white" viewBox="0 0 16 16">
-            <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z" />
-          </svg>
-        </div>
-        | Bancalux
-      </li>
-      <li>
-        <a href="../Controlador/cerrarSesionCliente.php" class="text-blue-500">Cerrar Sesión</a>
-      </li>
-    </ul>
-  </nav>
+    <nav class="bg-slate-950 bg-opacity-0 w-full p-4">
+        <ul class="flex justify-between items-center text-zinc-50 text-lg">
+            <li class="flex items-center font-bold text-2xl">
+                <div class="relative inline-block w-[2.5rem] h-[2.5rem] mr-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                        class="absolute inset-0 text-zinc-200 opacity-40 w-full h-full" viewBox="0 0 16 16">
+                        <path
+                            d="m8 0 6.61 3h.89a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v7a.5.5 0 0 1 .485.38l.5 2a.498.498 0 0 1-.485.62H.5a.498.498 0 0 1-.485-.62l.5-2A.5.5 0 0 1 1 13V6H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 3h.89zM3.777 3h8.447L8 1zM2 6v7h1V6zm2 0v7h2.5V6zm3.5 0v7h1V6zm2 0v7H12V6zM13 6v7h1V6zm2-1V4H1v1zm-.39 9H1.39l-.25 1h13.72z" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                        class="absolute inset-0 bottom-4 m-auto w-[1.2rem] h-[1.2rem] text-white" viewBox="0 0 16 16">
+                        <path
+                            d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z" />
+                    </svg>
+                </div>
+                | Bancalux
+            </li>
+            <li>
+                <a href="../Controlador/cerrarSesionCliente.php" class="text-blue-500">Cerrar Sesión</a>
+            </li>
+        </ul>
+    </nav>
 
-  <main class="w-full max-w-3xl mx-auto p-8">
-    <h2 class="text-3xl font-bold text-center text-white mb-6">¡Bienvenido/a!
-      <br>
-      <?php foreach ($resultados2 as $fila) {
+    <main class="w-full max-w-3xl mx-auto p-8">
+        <h2 class="text-3xl font-bold text-center text-white mb-6">¡Bienvenido/a!
+            <br>
+            <?php foreach ($resultados2 as $fila) {
         echo $fila['nombre'] . ' ' . $fila['apellido'];
         break;
       } ?>
-    </h2>
+        </h2>
 
-    <!-- Alertas -->
-    <?php
+        <!-- Alertas -->
+        <?php
       if (isset($_GET['opcion'])) {
         echo '<div class="mt-6 text-center text-red-500">Error: Has alcanzado el limite máximo de cuentas</div>';
       }
@@ -103,62 +109,72 @@ $resultados2 = $stmt2 -> fetchAll(PDO::FETCH_ASSOC);
       }
     ?>
 
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-8 mt-10">
-      <!-- Card 1 - Información de la Cuenta -->
-      <?php foreach ($resultados as $fila): ?>
-        <div class="bg-black/30 p-10 rounded-lg shadow-lg text-center transition-transform duration-300 hover:scale-105">
-          <h1 class="text-lg text-white font-bold"><?php echo $fila['numerocuenta']; ?></h1>
-          <p class="text-white">Nombre: <?php echo $fila['nombre'] . ' ' . $fila['apellido']; ?></p>
-          <p class="text-white">Saldo: $<?php echo number_format($fila['saldo'], 2); ?></p>
-          <p class="text-white">Fecha de Creación: <?php echo date("d-m-Y", strtotime($fila['fechaCrea'])); ?></p>
+<div class="flex flex-wrap justify-center mt-10">
+  <!-- Card 1 - Información de la Cuenta -->
+  <?php foreach ($resultados as $fila): ?>
+    <div class="bg-black/30 px-[10rem] py-[2rem] rounded-lg shadow-lg text-center m-3 transition-transform duration-300 hover:scale-105">
+      <h1 class="text-lg text-white font-bold">Num. Cuenta |  <?php echo $fila['numerocuenta']; ?></h1>
+      <p class="text-white mt-3">Nombre: <?php echo $fila['nombre'] . ' ' . $fila['apellido']; ?></p>
+      <p class="text-white">Saldo: $<?php echo number_format($fila['saldo'], 2); ?></p>
+      <p class="text-white">Fecha de Creación: <?php echo date("d-m-Y", strtotime($fila['fechaCrea'])); ?></p>
+    </div>
+  <?php endforeach; ?>
+</div>
+
+
+
+        <h2 class="text-3xl font-bold text-center text-white mb-6 mt-5">Más opciones</h2>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+
+            <!-- Card 3 - Registrar Clientes -->
+            <div
+                class="bg-black/30 backdrop-blur-lg p-6 rounded-lg shadow-lg text-center transition-transform duration-300 hover:scale-105">
+                <img src="Recursos/Imagenes/deposito.png" alt="Registrar Clientes" class="w-16 h-16 mx-auto mb-4">
+                <h1 class="text-lg font-bold text-white">Crear Cuentas</h1>
+                <p class="text-zinc-50">Crea como maximo 3 cuentas de ahorro personales.</p>
+                <a href="crearCuentaBancaria.php?id=<?php echo $usuario ?>"
+                    class="mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600">
+                    Ingresar
+                </a>
+            </div>
+
+            <!-- Card 4 - Préstamos -->
+            <div
+                class="bg-black/30 backdrop-blur-lg p-6 rounded-lg shadow-lg text-center transition-transform duration-300 hover:scale-105">
+                <img src="Recursos/Imagenes/registro.png" alt="Prestamos" class="w-16 h-16 mx-auto mb-4">
+                <h1 class="text-lg font-bold text-white">Movimientos</h1>
+                <p class="text-zinc-50">Verifica y administra los movimientos de tus cuentas.</p>
+                <a href="historialTransacciones.php?id=<?php echo $usuario ?>"
+                    class="mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600">
+                    Ingresar
+                </a>
+            </div>
+
+            <div
+                class="bg-black/30 backdrop-blur-lg p-6 rounded-lg shadow-lg text-center transition-transform duration-300 hover:scale-105">
+                <img src="Recursos/Imagenes/prestamo.png" alt="Prestamos" class="w-16 h-16 mx-auto mb-4">
+                <h1 class="text-lg font-bold text-white">Transferencias</h1>
+                <p class="text-zinc-50">Envia direro a tus cuentas personales.</p>
+                <a href="realizarTransferencia.php?id=<?php echo $usuario ?>"
+                    class="mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600">
+                    Ingresar
+                </a>
+            </div>
+
+            <div
+                class="bg-black/30 backdrop-blur-lg p-6 rounded-lg shadow-lg text-center transition-transform duration-300 hover:scale-105">
+                <img src="Recursos/Imagenes/prestamo.png" alt="Prestamos" class="w-16 h-16 mx-auto mb-4">
+                <h1 class="text-lg font-bold text-white">Prestamos</h1>
+                <p class="text-zinc-50">Solicita/Apertura prestamos</p>
+                <a href="solicitarPrestamo.php?id=<?php echo $usuario ?>"
+                    class="mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600">
+                    Ingresar
+                </a>
+            </div>
         </div>
-      <?php endforeach; ?>
-    </div>
 
-    <h2 class="text-3xl font-bold text-center text-white mb-6 mt-5">Más opciones</h2>
-
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-
-      <!-- Card 3 - Registrar Clientes -->
-      <div class="bg-black/30 backdrop-blur-lg p-6 rounded-lg shadow-lg text-center transition-transform duration-300 hover:scale-105">
-        <img src="Recursos/Imagenes/deposito.png" alt="Registrar Clientes" class="w-16 h-16 mx-auto mb-4">
-        <h1 class="text-lg font-bold text-white">Crear Cuentas</h1>
-        <p class="text-zinc-50">Crea como maximo 3 cuentas de ahorro personales.</p>
-        <a href="crearCuentaBancaria.php?id=<?php echo $usuario ?>" class="mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600">
-          Ingresar
-        </a>
-      </div>
-
-      <!-- Card 4 - Préstamos -->
-      <div class="bg-black/30 backdrop-blur-lg p-6 rounded-lg shadow-lg text-center transition-transform duration-300 hover:scale-105">
-        <img src="Recursos/Imagenes/registro.png" alt="Prestamos" class="w-16 h-16 mx-auto mb-4">
-        <h1 class="text-lg font-bold text-white">Movimientos</h1>
-        <p class="text-zinc-50">Verifica y administra los movimientos de tus cuentas.</p>
-        <a href="historialTransacciones.php?id=<?php echo $usuario ?>" class="mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600">
-          Ingresar
-        </a>
-      </div>
-
-      <div class="bg-black/30 backdrop-blur-lg p-6 rounded-lg shadow-lg text-center transition-transform duration-300 hover:scale-105">
-        <img src="Recursos/Imagenes/prestamo.png" alt="Prestamos" class="w-16 h-16 mx-auto mb-4">
-        <h1 class="text-lg font-bold text-white">Transferencias</h1>
-        <p class="text-zinc-50">Envia direro a tus cuentas personales.</p>
-        <a href="realizarTransferencia.php?id=<?php echo $usuario ?>" class="mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600">
-          Ingresar
-        </a>
-      </div>
-
-      <div class="bg-black/30 backdrop-blur-lg p-6 rounded-lg shadow-lg text-center transition-transform duration-300 hover:scale-105">
-        <img src="Recursos/Imagenes/prestamo.png" alt="Prestamos" class="w-16 h-16 mx-auto mb-4">
-        <h1 class="text-lg font-bold text-white">Prestamos</h1>
-        <p class="text-zinc-50">Solicita/Apertura prestamos</p>
-        <a href="solicitarPrestamo.php?id=<?php echo $usuario ?>" class="mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600">
-          Ingresar
-        </a>
-      </div>
-    </div>
-
-  </main>
+    </main>
 
 </body>
 
